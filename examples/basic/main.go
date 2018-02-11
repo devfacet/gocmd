177508f84de80a079cf8224746dabb2bcd910515
Fatih Cetinkaya <devfacet@gmail.com>
2018-02-10 23:09:33 -0500

    Update example app

--- examples/basic/main.go
+++ examples/basic/main.go
@@ -4,7 +4,7 @@ package main
 import (
 	"fmt"
 	"log"
-	"strings"
+	"math"
 
 	"github.com/devfacet/gocmd"
 )
@@ -14,8 +14,15 @@ func main() {
 		Help      bool `short:"h" long:"help" description:"Display usage" global:"true"`
 		Version   bool `short:"v" long:"version" description:"Display version"`
 		VersionEx bool `long:"vv" description:"Display version (extended)"`
-		Echo      struct {
-		} `command:"echo" description:"Print arguments"`
+		Math      struct {
+			Sqrt struct {
+				Number float64 `short:"n" long:"number" required:"true" description:"Number"`
+			} `command:"sqrt" description:"Calculate square root"`
+			Pow struct {
+				Base     float64 `short:"b" long:"base" required:"true" description:"Base"`
+				Exponent float64 `short:"e" long:"exponent" required:"true" description:"Exponent"`
+			} `command:"pow" description:"Calculate base exponential"`
+		} `command:"math" description:"Math functions"`
 	}{}
 
 	cmd, err := gocmd.New(gocmd.Options{
@@ -25,16 +32,20 @@ func main() {
 		Flags:       &flags,
 		AutoHelp:    true,
 		AutoVersion: true,
+		AnyError:    true,
 	})
 	if err != nil {
 		log.Fatal(err)
-	} else if len(cmd.FlagErrors()) > 0 {
-		log.Fatal(cmd.FlagErrors()[0])
 	}
 
-	// Echo
-	if args, ok := cmd.LookupFlag("Echo"); ok && args != nil {
-		fmt.Printf("%s\n", strings.TrimRight(strings.TrimLeft(fmt.Sprintf("%v", args[1:]), "["), "]"))
+	if cmd.FlagArgs("Math") != nil {
+		if cmd.FlagArgs("Math.Sqrt") != nil {
+			fmt.Println(math.Sqrt(flags.Math.Sqrt.Number))
+		} else if cmd.FlagArgs("Math.Pow") != nil {
+			fmt.Println(math.Pow(flags.Math.Pow.Base, flags.Math.Pow.Exponent))
+		} else {
+			log.Fatal("invalid math command")
+		}
 		return
 	}
 }
