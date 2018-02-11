b8a3490908497e99a946a267f9288433b5a65b35
Fatih Cetinkaya <devfacet@gmail.com>
2018-02-10 23:09:49 -0500

    Improve test examples

--- gocmd_test.go
+++ gocmd_test.go
@@ -7,6 +7,9 @@ package gocmd_test
 
 import (
 	"errors"
+	"fmt"
+	"log"
+	"math"
 	"os"
 	"testing"
 
@@ -28,7 +31,6 @@ func resetArgs() {
 
 func TestNew(t *testing.T) {
 	Convey("should create a new command", t, func() {
-		// Override the command line arguments for the test
 		resetArgs()
 		os.Args = append(os.Args, "-f=true", "--bar=test", "-b", "baz", "test", "-f=false", "-b=bar")
 
@@ -57,9 +59,13 @@ func TestNew(t *testing.T) {
 		So(flags.Baz, ShouldEqual, "baz")
 		So(flags.Command.Foo, ShouldEqual, false)
 		So(flags.Command.Bar, ShouldEqual, "bar")
+
+		resetArgs()
 	})
 
 	Convey("should fail to create a new command", t, func() {
+		resetArgs()
+
 		cmd, err := gocmd.New(gocmd.Options{
 			Name:        "test",
 			Version:     "1.0.0",
@@ -71,6 +77,21 @@ func TestNew(t *testing.T) {
 		So(err, ShouldNotBeNil)
 		So(err, ShouldBeError, errors.New("short argument foo in Foo field must be one character long"))
 		So(cmd, ShouldBeNil)
+
+		cmd, err = gocmd.New(gocmd.Options{
+			Name:        "test",
+			Version:     "1.0.0",
+			Description: "Test",
+			Flags: &struct {
+				Foo bool `short:"f" required:"true"`
+			}{},
+			AnyError: true,
+		})
+		So(err, ShouldNotBeNil)
+		So(err, ShouldBeError, errors.New("argument -f is required"))
+		So(cmd, ShouldBeNil)
+
+		resetArgs()
 	})
 }
 
@@ -125,6 +146,8 @@ func TestCmd_LookupFlag(t *testing.T) {
 		v, ok = cmd.LookupFlag("Bar")
 		So(v, ShouldBeNil)
 		So(ok, ShouldEqual, false)
+
+		resetArgs()
 	})
 }
 
@@ -146,6 +169,29 @@ func TestCmd_FlagValue(t *testing.T) {
 		v, ok = cmd.FlagValue("Bar").(bool)
 		So(v, ShouldEqual, false)
 		So(ok, ShouldEqual, false)
+
+		resetArgs()
+	})
+}
+
+func TestCmd_FlagArgs(t *testing.T) {
+	Convey("should return the correct flag args", t, func() {
+		resetArgs()
+		os.Args = append(os.Args, "-f=true")
+
+		cmd, err := gocmd.New(gocmd.Options{
+			Flags: &struct {
+				Foo bool `short:"f" default:"true"`
+			}{},
+		})
+		So(err, ShouldBeNil)
+		So(cmd, ShouldNotBeNil)
+		args := cmd.FlagArgs("Foo")
+		So(args, ShouldHaveLength, 1)
+		args = cmd.FlagArgs("Bar")
+		So(args, ShouldHaveLength, 0)
+
+		resetArgs()
 	})
 }
 
@@ -165,86 +211,135 @@ func TestCmd_FlagErrors(t *testing.T) {
 	})
 }
 
-func ExampleCmd_PrintVersion() {
-	cmd, _ := gocmd.New(gocmd.Options{
-		Version: "1.0.0",
-	})
-	cmd.PrintVersion(false)
-	// Output: 1.0.0
-}
+func ExampleNew_usage() {
+	os.Args = []string{"gocmd.test"}
 
-func ExampleCmd_PrintVersion_auto() {
-	os.Args = []string{"gocmd.test", "-v"}
-	gocmd.New(gocmd.Options{
-		Version: "1.0.0",
-		Flags: &struct {
-			Version bool `short:"v" long:"version"`
-		}{},
-		AutoVersion: true,
-	})
-	resetArgs()
-	// Output: 1.0.0
-}
-
-func ExampleCmd_PrintVersion_extra() {
-	cmd, _ := gocmd.New(gocmd.Options{
-		Name:    "test",
-		Version: "1.0.0",
-	})
-	cmd.PrintVersion(true)
-	// Output:
-	// App name    : test
-	// App version : 1.0.0
-	// Go version  : TEST
-}
-
-func ExampleCmd_PrintVersion_extra_auto() {
-	os.Args = []string{"gocmd.test", "-vv"}
-	gocmd.New(gocmd.Options{
-		Name:    "test",
-		Version: "1.0.0",
-		Flags: &struct {
-			Version bool `long:"vv" long:"version"`
-		}{},
-		AutoVersion: true,
-	})
-	resetArgs()
-	// Output:
-	// App name    : test
-	// App version : 1.0.0
-	// Go version  : TEST
-}
-
-func ExampleCmd_PrintUsage() {
-	cmd, _ := gocmd.New(gocmd.Options{
-		Name:        "test",
+	_, err := gocmd.New(gocmd.Options{
+		Name:        "basic",
 		Version:     "1.0.0",
-		Description: "Test",
-	})
-	cmd.PrintUsage()
-	// Output:
-	// Usage: test
-	//
-	// Test
-}
-
-func ExampleCmd_PrintUsage_auto() {
-	os.Args = []string{"gocmd.test", "-h"}
-	gocmd.New(gocmd.Options{
-		Name:        "test",
-		Version:     "1.0.0",
-		Description: "Test",
+		Description: "A basic app",
 		Flags: &struct {
-			Help bool `short:"h" long:"help"`
+			Help      bool `short:"h" long:"help" description:"Display usage" global:"true"`
+			Version   bool `short:"v" long:"version" description:"Display version"`
+			VersionEx bool `long:"vv" description:"Display version (extended)"`
+			Math      struct {
+				Sqrt struct {
+					Number float64 `short:"n" long:"number" required:"true" description:"Number"`
+				} `command:"sqrt" description:"Calculate square root"`
+				Pow struct {
+					Base     float64 `short:"b" long:"base" required:"true" description:"Base"`
+					Exponent float64 `short:"e" long:"exponent" required:"true" description:"Exponent"`
+				} `command:"pow" description:"Calculate base exponential"`
+			} `command:"math" description:"Math functions"`
 		}{},
-		AutoHelp: true,
+		AutoHelp:    true,
+		AutoVersion: true,
+		AnyError:    true,
 	})
-	resetArgs()
+	if err != nil {
+		log.Fatal(err)
+	}
 	// Output:
-	// Usage: test [options...]
+	// Usage: basic [options...] COMMAND [options...]
 	//
-	// Test
+	// A basic app
 	//
 	// Options:
-	//   -h, --help
+	//   -h, --help         	Display usage
+	//   -v, --version      	Display version
+	//       --vv           	Display version (extended)
+	//
+	// Commands:
+	//   math               	Math functions
+	//     sqrt             	Calculate square root
+	//       -n, --number   	Number
+	//     pow              	Calculate base exponential
+	//       -b, --base     	Base
+	//       -e, --exponent 	Exponent
+
+	resetArgs()
+}
+
+func ExampleNew_version() {
+	os.Args = []string{"gocmd.test", "-vv"}
+
+	_, err := gocmd.New(gocmd.Options{
+		Name:        "basic",
+		Version:     "1.0.0",
+		Description: "A basic app",
+		Flags: &struct {
+			Help      bool `short:"h" long:"help" description:"Display usage" global:"true"`
+			Version   bool `short:"v" long:"version" description:"Display version"`
+			VersionEx bool `long:"vv" description:"Display version (extended)"`
+		}{},
+		AutoHelp:    true,
+		AutoVersion: true,
+		AnyError:    true,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Output:
+	// App name    : basic
+	// App version : 1.0.0
+	// Go version  : vTest
+
+	resetArgs()
+}
+
+func ExampleNew_command() {
+	os.Args = []string{"gocmd.test", "math", "sqrt", "-n=9"}
+
+	flags := struct {
+		Help      bool `short:"h" long:"help" description:"Display usage" global:"true"`
+		Version   bool `short:"v" long:"version" description:"Display version"`
+		VersionEx bool `long:"vv" description:"Display version (extended)"`
+		Math      struct {
+			Sqrt struct {
+				Number float64 `short:"n" long:"number" required:"true" description:"Number"`
+			} `command:"sqrt" description:"Calculate square root"`
+			Pow struct {
+				Base     float64 `short:"b" long:"base" required:"true" description:"Base"`
+				Exponent float64 `short:"e" long:"exponent" required:"true" description:"Exponent"`
+			} `command:"pow" description:"Calculate base exponential"`
+		} `command:"math" description:"Math functions"`
+	}{}
+
+	cmd, err := gocmd.New(gocmd.Options{
+		Name:        "basic",
+		Version:     "1.0.0",
+		Description: "A basic app",
+		Flags:       &flags,
+		AutoHelp:    true,
+		AutoVersion: true,
+		AnyError:    true,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if cmd.FlagArgs("Math") != nil {
+		if cmd.FlagArgs("Math.Sqrt") != nil {
+			fmt.Println(math.Sqrt(flags.Math.Sqrt.Number))
+		} else if cmd.FlagArgs("Math.Pow") != nil {
+			fmt.Println(math.Pow(flags.Math.Pow.Base, flags.Math.Pow.Exponent))
+		} else {
+			log.Fatal("invalid math command")
+		}
+		return
+	}
+	// Output:
+	// 3
+
+	resetArgs()
+}
+
+func ExampleCmd_PrintVersion() {
+	cmd, err := gocmd.New(gocmd.Options{
+		Version: "1.0.0",
+	})
+	if err == nil {
+		cmd.PrintVersion(false)
+	}
+	// Output: 1.0.0
 }
