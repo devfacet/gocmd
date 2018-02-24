e0b33d7d47f5cf2f806e4011f17fbd260e4d530f
Fatih Cetinkaya <devfacet@users.noreply.github.com>
2018-02-24 14:12:57 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -7,9 +7,13 @@ go get github.com/golang/lint/golint
 go get github.com/smartystreets/goconvey
 
 # Format, lint, check
-OUT=`gofmt -l . | (grep -v '^vendor\/' || true)`; if [ "$OUT" ]; then echo "gofmt: $OUT"; exit 1; fi
-OUT=`golint ./... | (grep -v '^vendor\/' || true)`; if [ "$OUT" ]; then echo "golint: $OUT"; exit 1; fi
-OUT=`find . -type f -name '*.go' | grep -v -E '^./vendor' | xargs -L1 dirname | uniq | xargs go vet`; if [ "$OUT" ]; then echo "govet: $OUT"; exit 1; fi
+FMT=`find . -type f -name '*.go' | grep -v -E '^./vendor' | xargs -L1 dirname | uniq | xargs gofmt -l`; if [ "$FMT" ]; then echo -e "fmt:\n$FMT"; fi
+LINT=`find . -type f -name '*.go' | grep -v -E '^./vendor' | xargs -L1 dirname | uniq | xargs golint`; if [ "$LINT" ]; then echo -e "lint:\n$LINT"; fi
+VET=`find . -type f -name '*.go' | grep -v -E '^./vendor' | xargs -L1 dirname | uniq | xargs go vet`; if [ "$VET" ]; then echo -e "vet:\n$VET"; fi
+
+if [[ "$FMT" || "$LINT" || "$VET" ]]; then
+  exit 1
+fi
 
 # Test
 go test -v ./...
