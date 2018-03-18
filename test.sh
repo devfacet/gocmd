b4cd0044800b9a888540dfcb3e02e94cb845cb8d
Fatih Cetinkaya <devfacet@gmail.com>
2018-03-18 12:30:36 -0400

    Tidy

--- test.sh
+++ test.sh
@@ -10,7 +10,6 @@ go get github.com/smartystreets/goconvey
 FMT=`find . -type f -name '*.go' | grep -v -E '^./vendor' | xargs -L1 dirname | uniq | xargs gofmt -l`; if [ "$FMT" ]; then echo -e "fmt:\n$FMT"; fi
 LINT=`find . -type f -name '*.go' | grep -v -E '^./vendor' | xargs -L1 dirname | uniq | xargs golint`; if [ "$LINT" ]; then echo -e "lint:\n$LINT"; fi
 VET=`find . -type f -name '*.go' | grep -v -E '^./vendor' | xargs -L1 dirname | uniq | xargs go vet`; if [ "$VET" ]; then echo -e "vet:\n$VET"; fi
-
 if [[ "$FMT" || "$LINT" || "$VET" ]]; then
   exit 1
 fi
