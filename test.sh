ae1d624aa7074f1ad0152aded9e9a93272586c89
Fatih Cetinkaya <devfacet@gmail.com>
2018-09-04 23:15:41 -0400

    Update gofmt command

--- test.sh
+++ test.sh
@@ -7,7 +7,7 @@ go get github.com/golang/lint/golint
 go get github.com/smartystreets/goconvey
 
 # Format, lint, check
-FMT=`find . -type f -name '*.go' | grep -v -E '^./vendor' | xargs -L1 dirname | uniq | xargs gofmt -l`; if [ "$FMT" ]; then echo -e "fmt:\n$FMT"; fi
+FMT=`(gofmt -l . | grep -v -E '^vendor/') || true`; if [ "$FMT" ]; then echo -e "fmt:\n$FMT"; fi
 LINT=`find . -type f -name '*.go' | grep -v -E '^./vendor' | xargs -L1 dirname | uniq | xargs golint`; if [ "$LINT" ]; then echo -e "lint:\n$LINT"; fi
 VET=`find . -type f -name '*.go' | grep -v -E '^./vendor' | xargs -L1 dirname | uniq | xargs go vet`; if [ "$VET" ]; then echo -e "vet:\n$VET"; fi
 if [[ "$FMT" || "$LINT" || "$VET" ]]; then
